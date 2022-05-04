1ae072281ba7d4cc7ebb79f093d9e487543351ef
bot0007 <samanjungshah6781@gmail.com>
2022-05-04 18:27:06 +0545

    basic Todo list Node app

---
+++ index.js
@@ -0,0 +1,37 @@
+const  express = require('express');
+const app  = express();
+const bodyParser = require('body-parser');
+
+app.set('view engine','ejs');
+app.use(bodyParser.urlencoded({urlencoded:true}));
+var newItem = [];
+
+app.get('/',(req,res)=>{
+
+
+    var today = new Date();
+    var currentDay = today.getDay();
+    var options = {
+        weekday:"long",
+        day:"numeric",
+        month:"long"
+    }
+    var day = today.toLocaleDateString("en-US",options);
+
+    
+    res.render('list',{kindOfDay:day,newListItem:newItem});      
+
+
+})
+
+app.post('/',(req,res)=>{
+    var item = req.body.newItem;
+    newItem.push(item);
+
+    res.redirect("/");
+})
+
+
+app.listen(process.env.PORT || 3000,()=>{
+    console.log('Server started on port 3000');
+});
